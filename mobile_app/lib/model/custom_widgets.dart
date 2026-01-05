a4703e1c309f5d6788171fdc59debed956e8480e
hamster336 <aashukalauni12@gmail.com>
2026-01-05 21:01:26 +0545

    Notices Screen UI

--- mobile_app/lib/model/custom_widgets.dart
+++ mobile_app/lib/model/custom_widgets.dart
@@ -78,13 +78,8 @@ class CustomWidgets {
                   padding: const EdgeInsets.all(8),
 
                   decoration: BoxDecoration(
-                    // color: Color(0xFF2AB3AA),
+                    color: Color(0xFF2AB3AA),
                     shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      colors: <Color>[Color(0xFF3B8D9B), Color(0xFF00FFB7)],
-                      begin: .topLeft,
-                      end: .bottomRight,
-                    ),
                   ),
                   child: Icon(icon, size: 27, color: Colors.white),
                 ),
@@ -128,7 +123,7 @@ class CustomWidgets {
         child: ListTile(
           title: Text(notice.title, style: TextStyle(fontSize: 20)),
           subtitle: Text(
-            formatTime(notice.publishedAt),
+            formatTimeForHome(notice.publishedAt),
             style: TextStyle(color: Colors.black54),
           ),
         ),
@@ -137,24 +132,148 @@ class CustomWidgets {
   }
 
   // format time for homescreen notice card
-  static String formatTime(DateTime publishedAt) {
+  static String formatTimeForHome(DateTime publishedAt) {
     final now = DateTime.now();
-    final date = DateTime(publishedAt.year, publishedAt.month, publishedAt.day);
-    final today = DateTime(now.year, now.month, now.day);
-    final difference = today.difference(date).inDays;
+    final difference = now.difference(publishedAt).inDays;
 
     if (difference == 0) {
-      return 'Today, ${DateFormat('hh:mm a').format(date)}';
+      return 'Today, ${DateFormat('hh:mm a').format(publishedAt)}';
     }
 
     if (difference == 1) {
-      return 'Yesterday, ${DateFormat('hh:mm a').format(date)}';
+      return 'Yesterday, ${DateFormat('hh:mm a').format(publishedAt)}';
     }
 
     if (difference <= 7) {
-      return DateFormat('EEEE, hh:mm a').format(date);
+      return DateFormat('EEEE, hh:mm a').format(publishedAt);
     }
 
-    return DateFormat('dd MMM, hh:mm a').format(date);
+    return DateFormat('dd MMM, hh:mm a').format(publishedAt);
+  }
+
+  // filter buttons
+  static Widget filterButton(
+    String label,
+    NoticeFilter filter,
+    NoticeFilter currentFilter,
+  ) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: (filter == currentFilter) ? Color(0xFF2AB3AA) : null,
+        shadowColor: Colors.transparent,
+        elevation: 6,
+      ),
+      child: Text(
+        label,
+        style: TextStyle(
+          fontSize: 16,
+          color: (filter == currentFilter) ? Colors.white : null,
+        ),
+      ),
+    );
+  }
+
+  // notice card for notice screen
+  static Widget noticeCard(Notice notice) {
+    Color cardColor = Colors.blue;
+    String priority = 'Info';
+
+    if (notice.priority == InfoPriority.urgent) {
+      cardColor = Colors.red;
+      priority = 'Urgent';
+    } else if (notice.priority == InfoPriority.important) {
+      cardColor = Colors.yellow.shade700;
+      priority = 'Important';
+    }
+
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        color: cardColor,
+      ),
+      child: Card(
+        margin: const EdgeInsets.fromLTRB(5, 0, 0, 0),
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          child: Column(
+            crossAxisAlignment: .start,
+            children: [
+              Row(
+                mainAxisAlignment: .spaceBetween,
+                children: [
+                  Flexible(
+                    child: Text(
+                      notice.title,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      overflow: .visible,
+                    ),
+                  ),
+
+                  const SizedBox(width: 5),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      color: cardColor.withValues(alpha: 0.2),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(priority, style: TextStyle(color: cardColor)),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 5),
+              Text(
+                'Posted ${formatTime(notice.publishedAt)}',
+                style: TextStyle(color: Colors.black45),
+              ),
+
+              Text(
+                notice.description,
+                style: TextStyle(fontSize: 17, color: Colors.black54),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // format time for notice cards
+  static String formatTime(DateTime publishedAt) {
+    final now = DateTime.now();
+    final diff = now.difference(publishedAt);
+
+    // if (diff.isNegative) {
+    //   return 'on ${DateFormat('dd MMM y').format(publishedAt)}';
+    // }
+
+    if (diff.inSeconds < 60) return 'now';
+
+    if (diff.inMinutes < 60) {
+      final m = diff.inMinutes;
+      return '$m minute${m == 1 ? '' : 's'} ago';
+    }
+
+    if (diff.inHours < 24) {
+      final h = diff.inHours;
+      return '$h hour${h == 1 ? '' : 's'} ago';
+    }
+
+    if (diff.inDays < 30) {
+      final d = diff.inDays;
+      return '$d day${d == 1 ? '' : 's'} ago';
+    }
+
+    if (publishedAt.year < now.year) {
+      return 'on ${DateFormat('dd MMM y').format(publishedAt)}';
+    }
+
+    return 'on ${DateFormat('dd MMM').format(publishedAt)}';
   }
 }
--- mobile_app/lib/model/required_enums.dart
+++ mobile_app/lib/model/required_enums.dart
@@ -1,1 +1,5 @@
 enum InfoPriority { urgent, important, info }
+
+enum NoticeFilter { all, urgent, important }
+
+enum AssignmentFilter { pending, completed, overdue }
