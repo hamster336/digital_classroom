952fd611fb9ea6894b2187ca28b65a8c2730e839
Bishal Mahar <bishalmahar4@gmail.com>
2026-02-10 00:02:47 +0545

    authentication

---
+++ admin_web/supabase/supabase-client.js
@@ -0,0 +1,6 @@
+import {createClient} from "@supabase/supabase-js"
+
+export const supabase = createClient(
+  "https://ksebauxqprpstdvmwwse.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzZWJhdXhxcHJwc3Rkdm13d3NlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc5Njc3MDIsImV4cCI6MjA4MzU0MzcwMn0.ze_g3A3NdAVGp56q_dU75mo7o921HgwCTTdNp6K6FFY"
+);
