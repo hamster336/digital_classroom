27d4393d70d36689b7c152829c730f59c1677aa1
Bishal Mahar <bishalmahar4@gmail.com>
2026-02-10 23:11:48 +0545

    crud operation

---
+++ admin_web/supabase/signup.js
@@ -0,0 +1,17 @@
+import { supabase } from "./client";
+
+export const signUp = async (email, password) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+
+  if (error) {
+    throw error;
+  }
+
+  return {
+    user: data.user,
+    session: data.session,
+  };
+};
